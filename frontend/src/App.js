86fb9a1f6bd4d95dfb6dd6d7f4f95694b6a01bda
NIKITHA U P <136329098+NikithaUP@users.noreply.github.com>
2024-11-11 23:14:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 import axios from 'axios';
 import AddTask from './components/addTask/AddTask';
 import TodoList from './components/todoList/TodoList';
@@ -7,65 +7,52 @@ import UpdateTask from './components/updateTask/UpdateTask';
 
 function App() {
   const [todolist, setTodolist] = useState([]);
-  const [taskUpdate, setTaskUpdate] = useState({});
-  const [showPopup, setShowPopup] = useState(false);
-  const [filter, setFilter] = useState('all'); // 'all', 'completed', 'unfinished'
-
+  const [taskUpdate, setTaskUpdate] = useState({})
+  const [showPopup,setShowPopup]=useState(false)
   useEffect(() => {
-    axios.get(`${process.env.BASE_URL}/api/tasks`)
-      .then(res => setTodolist(res.data))
-      .catch(err => console.log(err));
-  }, []);
-
-  const addTask = newTask => setTodolist([...todolist, newTask]);
+    axios.get(`${process.env.BASE_URL}/api/tasks`).then(res => {
+      setTodolist(res.data)
+    }).catch(err => console.log(err))
+  }, [])
+  
+  const addTask = newTask => {
+    setTodolist([...todolist,newTask])
+  }
 
   const taskComplete = task => {
-    const updatedList = todolist.map(item =>
-      item._id === task._id ? { ...item, isComplete: task.isComplete } : item
-    );
-    setTodolist(updatedList);
-  };
+    const newList = [...todolist]
+    newList.forEach(item => {
+      if (item._id === task._id) {
+        item.isComplete=task.isComplete
+      }
+    })
+    setTodolist(newList)
+  }
 
-  const removeTask = task => setTodolist(todolist.filter(item => item._id !== task._id));
-
+  const removeTask = task => {
+    const newList = todolist.filter(item => !(item._id === task._id))
+    setTodolist(newList);
+  }
+  
   const updatetask = task => {
-    const updatedList = todolist.map(item =>
-      item._id === task._id ? { ...item, todo: task.todo } : item
-    );
-    setTodolist(updatedList);
-  };
-
-  // Filter tasks based on the selected filter
-  const filteredTasks = todolist.filter(task => {
-    if (filter === 'completed') return task.isComplete;
-    if (filter === 'unfinished') return !task.isComplete;
-    return true; // 'all' filter shows all tasks
-  });
+    const newList = [...todolist]
+    newList.forEach(item => {
+      if (item._id === task._id) {
+        item.todo=task.todo
+      }
+    })
+    setTodolist(newList)
+  }
 
   return (
-    <div className="App">
+    <div>
       <AddTask addTask={addTask} />
-      <div className="filter-buttons">
-        <button onClick={() => setFilter('all')}>All Todos</button>
-        <button onClick={() => setFilter('completed')}>Completed Todos</button>
-        <button onClick={() => setFilter('unfinished')}>Unfinished Todos</button>
-      </div>
-      <TodoList
-        todolist={filteredTasks}
-        taskComplete={taskComplete}
-        removeTask={removeTask}
-        taskUpdate={task => setTaskUpdate(task)}
-        showPopup={() => setShowPopup(!showPopup)}
-      />
-      {showPopup && (
-        <UpdateTask
-          task={taskUpdate}
-          updatetask={updatetask}
-          removePopup={() => setShowPopup(!showPopup)}
-        />
-      )}
+      <TodoList todolist={todolist} taskComplete={taskComplete} removeTask={removeTask}
+        taskUpdate={task =>setTaskUpdate(task)} showPopup={()=>setShowPopup(!showPopup)} />
+      {showPopup && <UpdateTask task={taskUpdate} updatetask={updatetask}
+        removePopup={()=>setShowPopup(!showPopup)} />}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
